d5281766501190ee7668db43367db8c56d3c3768
Marcio Rogerio Nizzola <marcio@nizzola.com.br>
2022-06-11 11:27:03 -0300

    set logging configuration

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using WebAPI;
 using WebAPI.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureDI();
-
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.RequestPropertiesAndHeaders |
+                            HttpLoggingFields.ResponsePropertiesAndHeaders |
+                            HttpLoggingFields.ResponseBody |
+                            HttpLoggingFields.RequestBody;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var app = builder.Build();
 
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console());
+
+var app = builder.Build();
+app.UseHttpLogging();
 app.AddEndpoints();
 
 app.UseSwagger();
